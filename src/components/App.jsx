d8828e1a307c3fab8ab266440289be9c5f4e889d
Christian Duda <89409014+DudaKristian@users.noreply.github.com>
2022-07-07 00:12:56 +0300

    finished

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,14 @@
 import Profile from "./Profile/Profile"
 import user from "./Profile/user.json"
 import mainStyle  from "./mainStyle.module.css"
+import Statistics from "./Statistics/Statistics.jsx";
+import statisticsData from "./Statistics/data.json"
+import FriendList from "./FriendList/FriendList"
+import friends from "./FriendList/friends.json"
+import TransactionHistory from "./Transactions/Transactions.jsx"
+import transactions from  "./Transactions/transactions.json"
+
+
 
 export const App = () => {
   return (
@@ -13,6 +21,15 @@ export const App = () => {
       location={user.location}
       avatar={user.avatar}
       />
+
+      <Statistics
+      title = "Upload stats"
+      stats={statisticsData}
+      />
+
+      <FriendList friends={friends} />;
+
+      <TransactionHistory items={transactions} />;
     </div>
   );
 };
---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,36 @@
+import PropTypes from "prop-types"
+import styles from "./friendList.module.css"
+
+const FriendList = ({ friends } ) => {
+    return (
+        <div>
+            <ul className = {styles.friendList}>
+                {friends.map(({ avatar, name, isOnline, id }) => (
+                    <li key = {id} className = {styles.item}>
+                        <span   className={styles.status}
+                            style={isOnline ?
+                                { backgroundColor: "green" } :
+                                { backgroundColor: "red" }} >
+                            {isOnline}</span>
+                        <img src={avatar} alt="User avatar" width="48" className={styles.avatar} />
+                        <p className={styles.name}>{name}</p>
+                    </li>))
+                }
+            </ul>
+        </div>
+    )
+}
+
+FriendList.propTypes = {
+    friends: PropTypes.arrayOf(
+        PropTypes.shape({
+        avatar: PropTypes.string.isRequired,
+        name: PropTypes.string.isRequired,
+        isOnline: PropTypes.bool.isRequired,
+        })
+    ),
+};
+
+
+
+export default FriendList
